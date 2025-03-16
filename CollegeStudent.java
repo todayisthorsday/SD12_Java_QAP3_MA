0fb3ee24d927ec9a17d4188549d7f77132a482f8
melanie <114537207+todayisthorsday@users.noreply.github.com>
2025-03-16 16:35:21 -0230

    created CollegeStudent class

---
+++ CollegeStudent.java
@@ -0,0 +1,32 @@
+public class CollegeStudent {
+    private String major;
+    private int year;
+
+    public CollegeStudent(String name, int age, String gender, String idNum, double gpa, int year, String major) {
+        super(name, age, gender, idNum, gpa);
+        this.year = year;
+        this.major = major;
+    }
+
+    // Getters & Setters
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Year: " + year + ", Major: " + major;
+    }
+}
