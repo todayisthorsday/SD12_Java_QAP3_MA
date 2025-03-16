d75992de8d9bf5426a9168e6c7c221c5e27d4a4c
melanie <114537207+todayisthorsday@users.noreply.github.com>
2025-03-16 18:49:32 -0230

    added moveable point & demo classes

---
+++ Problem-2/MoveablePoint.java
@@ -0,0 +1,56 @@
+public class MoveablePoint extends Point {
+    private float xSpeed;
+    private float ySpeed;
+
+    public MoveablePoint(float x, float y, float xSpeed, float ySpeed) {
+        super(x, y);
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public MoveablePoint(float xSpeed, float ySpeed) {
+        this(0.0f, 0.0f, xSpeed, ySpeed);
+    }
+
+    public MoveablePoint() {
+        this(0.0f, 0.0f);
+    }
+
+    // Getters and Setters
+    public float getXSpeed() {
+        return xSpeed;
+    }
+
+    public void setXSpeed(float xSpeed) {
+        this.xSpeed = xSpeed;
+    }
+
+    public float getYSpeed() {
+        return ySpeed;
+    }
+
+    public void setYSpeed(float ySpeed) {
+        this.ySpeed = ySpeed;
+    }
+
+    public void setSpeed(float xSpeed, float ySpeed) {
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public float[] getSpeed() {
+        return new float[] { xSpeed, ySpeed };
+    }
+
+    public MoveablePoint move() {
+        setX(getX() + xSpeed);
+        setY(getY() + ySpeed);
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + getX() + ", " + getY() + "), Speed= (" + xSpeed + ", " + ySpeed + ")";
+    }
+
+}
