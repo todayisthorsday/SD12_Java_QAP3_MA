83db400769533eaa0586cfd6fa9b769669e26a23
melanie <114537207+todayisthorsday@users.noreply.github.com>
2025-03-16 18:34:33 -0230

    created problem 2 folder and point class

---
+++ Problem-2/Point.java
@@ -0,0 +1,44 @@
+public class Point {
+    private float x;
+    private float y;
+
+    public Point(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point() {
+        this(0.0f, 0.0f);
+    }
+
+    // Getters and Setters
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public void setXY(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public float[] getXY() {
+        return new float[] { x, y };
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+}
