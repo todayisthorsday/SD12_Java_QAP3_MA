aac19544be81087fd074f2856807f533f6afbc51
melanie <114537207+todayisthorsday@users.noreply.github.com>
2025-03-16 16:27:47 -0230

    created teacher class

---
+++ Teacher.java
@@ -0,0 +1,32 @@
+public class Teacher extends Person {
+    private String subject;
+    private double salary;
+
+    public Teacher(String name, int age, String gender, String subject, double salary) {
+        super(name, age, gender);
+        this.subject = subject;
+        this.salary = salary;
+    }
+
+    // Getters & Setters
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Subject: " + subject + ", Salary: $" + salary;
+    }
+}
